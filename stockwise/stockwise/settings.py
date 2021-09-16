23410ca99896380b2335e7691b22bd99f683c81f
Alex Yom <alexyom24@gmail.com>
2021-09-15 22:21:00 -0400

    django configuration with updated security

--- stockwise/stockwise/settings.py
+++ stockwise/stockwise/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-juogl&@ce_h6^a3ldief%8303$*nd#joocsc$_3d_#-&d7urt2'
 
 ALLOWED_HOSTS = []
 
